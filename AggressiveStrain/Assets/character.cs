456b8a7abaf214fd372f303e85a88be512192dba
Josh <joshualeigh2002@gmail.com>
2025-01-20 14:59:34 +0000

    Project Added

---
+++ AggressiveStrain/Assets/character.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class character : MonoBehaviour
+{
+    private CharacterController characterController;
+    public Transform cam;
+    public float Speed = 5f;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        characterController.Move(move * Time.deltaTime * Speed);
+    }
+}
