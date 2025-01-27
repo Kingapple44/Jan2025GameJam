ea9ab90547e290eb8e38af1d101b0d3815e7a27f
Josh <joshualeigh2002@gmail.com>
2025-01-27 13:38:08 +0000

    Behaviour Tree Added and NavMesh working

--- AggressiveStrain/Assets/Enemies/Enemy.cs
+++ AggressiveStrain/Assets/Enemies/Enemy.cs
@@ -6,19 +6,15 @@ public class Enemy : MonoBehaviour
     public int Virus;
     public int Health;
     private float alpha;
-    public Transform target;
-    private NavMeshAgent agent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InvokeRepeating("VirusDamage", 1f, 1f);
-        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = target.position;
         if (Health < 0)
         {
             Destroy(gameObject);
